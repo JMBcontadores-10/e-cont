11a0f9ab32bd06700a368538eac064a6970bce52
octaviom97 <octaviom97@comunidad.unam.mx>
2021-06-16 16:41:31 -0500

    División de rutas en web.php

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Auth::routes(['register'=>false]);
 
+// Rutas Octavio
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 // Route::get('/registro', [App\Http\Controllers\Registro2Controller::class, 'index'])->name('registro');
 // Route::resource('/registro', 'App\Http\Controllers\Registro2Controller');
 Route::get('/descargasv2', [App\Http\Controllers\DescargasControllerv2::class, 'index'])->name('descargasv2');
 Route::post('/async', [App\Http\Controllers\Async::class, 'index'])->name('async');
 
+
+// Rutas Ana
