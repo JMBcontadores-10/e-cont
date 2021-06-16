30c213e19561d73f409a69beb684ec450f23f10f
Octavio <octaviom97@comunidad.unam.mx>
2021-06-16 10:29:47 -0500

    Actualización código 15-06-2021

--- resources/views/auth/registro.blade.php
+++ resources/views/auth/registro.blade.php
@@ -1,13 +1,13 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
-{{-- <div class="container">
+<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Registro Clientes') }}</div> --}}
+                <div class="card-header">{{ __('Registro Clientes') }}</div>
 
-                {{-- <div class="card-body">
+                <div class="card-body">
                     <form method="POST" action="{{ route('registro.store') }}">
                         @csrf --}}
 
@@ -219,19 +219,19 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+                        </div> --}}
 
-                        <div class="form-group row mb-0">
+                        {{-- <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Registrar') }}
                                 </button>
                             </div>
-                        </div>
-                    </form>
-                </div> --}}
-            {{-- </div>
+                        </div> --}}
+                    {{-- </form>
+                </div>
+            </div>
         </div>
     </div>
-</div> --}}
-@endsection
+</div>
+@endsection --}}
