00ff222afd91e4f1361fc4ab6a330bc7311a41d5
analagunas <aplagunitas@comunidad.unam.mx>
2021-08-30 11:04:23 -0500

    Tipo de usuarios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,63 +6,7 @@ use Illuminate\Support\Facades\DB;
 @extends('layouts.app')
 
 @section('content')
-    {{-- @if (Session::get('tipoU') == '2')
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row justify-content-center">
 
-                                <h2 style="color:#0055ff;"> Bienvenido Contador</h2>
-                            </div>
-                            <div class="row justify-content-center">
-                                <h5 style="color:#0055ff;">_____________________________________________________</h5>
-                            </div>
-                            <br>
-
-                            <form method="GET" action="{{ route('login') }}">
-                                @csrf
-                                <h4 style="text-align: center">Seleccione su empresa:</h4>
-                                @php
-
-                                    $cliente = DB::table('clientes')
-                                        ->where('RFC', $rfc)
-                                        ->first();
-                                    $empresas = $cliente['empresas'];
-                                    $cont = 0;
-                                    foreach ($empresas as $ef) {
-                                        $password[] = $ef['password'];
-                                    }
-                                @endphp
-                                <select name="RFC" id="empresas" class="form-control" onchange="variableP()">
-
-                                    @foreach ($empresas as $e)
-                                        <option value="{{ ++$cont }}.{{ $e['RFC'] }}">{{ $e['RFC'] }}</option>
-                                    @endforeach
-                                </select>
-
-
-
-                                <br>
-                                <br>
-                                <div class="form-group row mb-0">
-                                    <div class="col-md-8 offset-md-4">
-                                        <button type="submit" class="btn btn-primary">
-                                            {{ __('Enviar') }}
-                                        </button>
-
-                                    </div>
-                                </div>
-
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    @else --}}
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-8">
@@ -127,5 +71,5 @@ use Illuminate\Support\Facades\DB;
                 </div>
             </div>
         </div>
-    {{-- @endif --}}
+
 @endsection
